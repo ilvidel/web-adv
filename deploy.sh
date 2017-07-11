1483ec4d2927dd735934234d289c168734d1915c
ilvidel <ignacio.diez.arias@gmail.com>
2017-07-11 20:46:35 +0100

    desplegar tambien la seccion de playa

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ set -e
 sudo apt-get install -y lftp
 
 # deployment via ftp upload. Using FTPS for that
-lftp -c "set ftps:initial-prot ''; set ssl:verify-certificate false; set ftp:ssl-force true; set ftp:ssl-protect-data true; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
+lftp -c "set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv /home/nacho/repos/web-adv/public/ /www.advmiguelturra.org/; quit;"
+lftp -c "set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv /home/nacho/repos/web-adv/vplaya/ /www.advmiguelturra.org/vplaya; quit;"
