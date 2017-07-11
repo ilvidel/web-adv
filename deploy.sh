7400405c8789f3d72ccb57cbe31ba5e074851df6
ilvidel <ignacio.diez.arias@gmail.com>
2017-07-11 20:48:51 +0100

    no borrar archivos remotos

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 sudo apt-get install -y lftp
 
 # deployment via ftp upload. Using FTPS for that
-lftp -c "set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv /home/nacho/repos/web-adv/public/ /www.advmiguelturra.org/; quit;"
-lftp -c "set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv /home/nacho/repos/web-adv/vplaya/ /www.advmiguelturra.org/vplaya; quit;"
+lftp -c "set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -Rv /home/nacho/repos/web-adv/public/ /www.advmiguelturra.org/; quit;"
+lftp -c "set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -Rv /home/nacho/repos/web-adv/vplaya/ /www.advmiguelturra.org/vplaya; quit;"
