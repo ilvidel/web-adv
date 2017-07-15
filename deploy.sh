354611c4d64deb8c826b5fd545b7bae92544cc01
ilvidel <ignacio.diez.arias@gmail.com>
2017-07-15 21:37:49 +0100

    use passive mode and parallel jobs

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ sudo apt-get install -y lftp
 
 # deployment via ftp upload. Using FTPS for that
 echo "Updating the main website..."
-lftp -c "set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -Rv --only-missing --ignore-time public/ /www.advmiguelturra.org/; quit;"
+lftp -c "set ftp:passive-mode true; set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror --parallel=10 -Rv --only-missing --ignore-time public/ /www.advmiguelturra.org/; quit;"
 echo "Updating the vplaya section..."
-lftp -c "set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -Rv --only-missing --ignore-time vplaya/ /www.advmiguelturra.org/vplaya; quit;"
+lftp -c "set ftp:passive-mode true; set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror --parallel=10 -Rv --only-missing --ignore-time vplaya/ /www.advmiguelturra.org/vplaya; quit;"
